a120528b7d1ecfda858ec0f978444a33aedacf99
Will Soula <william.soula@drillinginfo.com>
2013-06-14 08:46:01 -0500

    Adding Alberto and script changes
    Adding Tropical Storm Alberto
    
    The scripts were changed to fail if the proper amount of values
    aren't passed to it.  Also made the convert to geojson output
    to a file.

--- convert-csv-to-geojson.sh
+++ convert-csv-to-geojson.sh
@@ -3,15 +3,20 @@ if [ -z $1 ]; then
   echo must enter an input file
   exit 1
 fi
-echo {
-echo \"type\": \"FeatureCollection\",
-echo \"features\": [
+if [ -z $2 ]; then
+  echo must enter a destination file
+  exit 1
+fi
+file=$2
+echo "{">$file
+echo "\"type\": \"FeatureCollection\",">>$file
+echo "\"features\": [">>$file
 while IFS=, read col1 col2 col3 col4 col5 col6 col7
 do
   #echo "I got:$col1|$col2|$col3|$col4|$col5|$col6|$col7"
-  echo { \"type\": \"Feature\", \"properties\": { \"ADV\": \"$col1\", \"LAT\": \"$col2\", \"LON\": \"$col3\", \"TIME\": \"$col4\", \"WIND\": \"$col5\", \"PR\": \"$col6\", \"STAT\": \"$col7\", \"NAME\": \"$2\", \"DATES\": \"$col4\" }, \"geometry\": { \"type\": \"Point\", \"coordinates\": [ $col3, $col2 ] } }
-  echo ","
+  echo "{ \"type\": \"Feature\", \"properties\": { \"ADV\": \"$col1\", \"LAT\": \"$col2\", \"LON\": \"$col3\", \"TIME\": \"$col4\", \"WIND\": \"$col5\", \"PR\": \"$col6\", \"STAT\": \"$col7\", \"NAME\": \"$2\", \"DATES\": \"$col4\" }, \"geometry\": { \"type\": \"Point\", \"coordinates\": [ $col3, $col2 ] } }">>$file
+  echo ",">>$file
 done < $1
-echo ""
-echo ]
-echo }
+echo "">>$file
+echo "]">>$file
+echo "}">>$file
--- convert-raw-data-to-csv.sh
+++ convert-raw-data-to-csv.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo must enter a file to convert
+  exit 1
+fi
+if [ -z $2 ]; then
+  echo must enter a filename to save as
+  exit 1
+fi
 cat $1 | sed -E "s%[ ]+%,%g" > $2.csv
