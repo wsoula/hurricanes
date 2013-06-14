b34cf6d18663df69245834b7b8d98e9485c32ca1
Will Soula <william.soula@drillinginfo.com>
2013-06-14 10:56:55 -0500

    Adding BERYL
    Added raw, csv, and geojson files for BERYL.  Also add script to
    get data from the website.

---
+++ getRawData.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo must enter a year first
+  exit 1
+fi
+if [ -z $2 ]; then
+  echo must enter a storm name second
+  exit 1
+fi
+year=$1
+name=$2
+wget http://weather.unisys.com/hurricane/atlantic/$year/$name/track.dat -O $year-$name-raw-data
